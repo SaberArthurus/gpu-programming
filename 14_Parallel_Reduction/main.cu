60ab34dc54ae6aa1e932793c15c1666181eb7598
Evgeny Agamirzov <eagamirzov@gmail.com>
2015-09-16 17:53:32 +0200

    Add reduction assignment

---
+++ 14_Parallel_Reduction/main.cu
@@ -0,0 +1,308 @@
+// ###
+// ###
+// ### Practical Course: GPU Programming in Computer Vision
+// ###
+// ###
+// ### Technical University Munich, Computer Vision Group
+// ### Summer Semester 2015, September 7 - October 6
+// ###
+// ###
+// ### Thomas Moellenhoff, Robert Maier, Caner Hazirbas
+// ###
+// ###
+// ###
+// ### THIS FILE IS SUPPOSED TO REMAIN UNCHANGED
+// ###
+// ###
+
+#include "aux.h"
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <cuda_runtime.h>
+#include "cublas_v2.h"
+
+using namespace std;
+
+#define SHARED_SIZE 49152
+#define BLOCK_SIZE_X 1024
+#define BLOCK_SIZE_Y 1
+
+
+// uncomment to use the camera
+//#define CAMERA
+
+
+__global__ void perform_reduction(float *input, float *sum, size_t size)
+{
+    float tmp = 0.0;
+    size_t i_g = threadIdx.x + blockDim.x * blockIdx.x, i_l = threadIdx.x;
+    extern __shared__ float sh_array[];
+
+    // Initialize shared memory
+    if (i_g < size)
+        tmp = input[i_g];
+        
+    sh_array[i_l] = tmp;
+
+    __syncthreads();
+
+    // Perform reduction
+    for (size_t offset = blockDim.x / 2; offset > 0; offset >>= 1)
+    {
+        if(i_l < offset)
+        {
+            sh_array[i_l] += sh_array[i_l + offset];
+        }
+        __syncthreads();
+    }
+
+    // Write result
+    if (i_l == 0)
+        atomicAdd(sum, sh_array[0]);
+}
+
+float cuda_reduction(float *d_imgIn, float size)
+{
+    /*----------------------------Assign block and grid-------------------------------------*/
+    // Initalization
+    float *d_sum, sum;
+    size_t shared_size, num_blocks;
+
+    // Block parameters
+    num_blocks = (size + BLOCK_SIZE_X - 1) / BLOCK_SIZE_X; 
+    shared_size = BLOCK_SIZE_X * sizeof(float);
+
+    // Set up the block and the grid
+    dim3 block = dim3(BLOCK_SIZE_X, 1, 1);
+    dim3 grid = dim3(num_blocks, 1, 1);
+
+
+    /*-------------------------------Allocate GPU memory-------------------------------------*/
+    cudaMalloc(&d_sum, sizeof(float)); CUDA_CHECK;
+    cudaMemset(d_sum, 0.0, sizeof(float));
+    
+    /*---------------------------------Kernel call------------------------------------------*/
+    perform_reduction <<<grid, block, shared_size>>> (d_imgIn, d_sum, size); CUDA_CHECK;
+
+
+    /*-------------------------------Copy from device---------------------------------------*/
+    cudaMemcpy(&sum, d_sum, sizeof(float), cudaMemcpyDeviceToHost); CUDA_CHECK;
+
+    // Free device memory
+    cudaFree(d_sum); CUDA_CHECK;
+
+    return sum;
+}
+
+
+int main(int argc, char **argv)
+{
+    // Before the GPU can process your kernels, a so called "CUDA context" must be initialized
+    // This happens on the very first call to a CUDA function, and takes some time (around half a second)
+    // We will do it right here, so that the run time measurements are accurate
+    cudaDeviceSynchronize();  CUDA_CHECK;
+
+
+
+
+    // Reading command line parameters:
+    // getParam("param", var, argc, argv) looks whether "-param xyz" is specified, and if so stores the value "xyz" in "var"
+    // If "-param" is not specified, the value of "var" remains unchanged
+    //
+    // return value: getParam("param", ...) returns true if "-param" is specified, and false otherwise
+
+#ifdef CAMERA
+#else
+    // input image
+    string image = "";
+    bool ret = getParam("i", image, argc, argv);
+    if (!ret) cerr << "ERROR: no image specified" << endl;
+    if (argc <= 1) { cout << "Usage: " << argv[0] << " -i <image> [-repeats <repeats>] [-gray]" << endl; return 1; }
+#endif
+    
+    // number of computation repetitions to get a better run time measurement
+    int repeats = 1;
+    getParam("repeats", repeats, argc, argv);
+    cout << "repeats: " << repeats << endl;
+    
+    // load the input image as grayscale if "-gray" is specifed
+    bool gray = false;
+    getParam("gray", gray, argc, argv);
+    cout << "gray: " << gray << endl;
+
+    // load the input image as grayscale if "-gray" is specifed
+    float gamma = 0.5;
+    getParam("gamma", gamma, argc, argv);
+    cout << "gamma: " << gamma << endl; 
+
+    // Init camera / Load input image
+#ifdef CAMERA
+
+    // Init camera
+  	cv::VideoCapture camera(0);
+  	if(!camera.isOpened()) { cerr << "ERROR: Could not open camera" << endl; return 1; }
+    int camW = 640;
+    int camH = 480;
+  	camera.set(CV_CAP_PROP_FRAME_WIDTH,camW);
+  	camera.set(CV_CAP_PROP_FRAME_HEIGHT,camH);
+    // read in first frame to get the dimensions
+    cv::Mat mIn;
+    camera >> mIn;
+    
+#else
+    
+    // Load the input image using opencv (load as grayscale if "gray==true", otherwise as is (may be color or grayscale))
+    cv::Mat mIn = cv::imread(image.c_str(), (gray? CV_LOAD_IMAGE_GRAYSCALE : -1));
+    // check
+    if (mIn.data == NULL) { cerr << "ERROR: Could not load image " << image << endl; return 1; }
+    
+#endif
+
+    // convert to float representation (opencv loads image values as single bytes by default)
+    mIn.convertTo(mIn,CV_32F);
+    // convert range of each channel to [0,1] (opencv default is [0,255])
+    mIn /= 255.f;
+    // get image dimensions
+    int w = mIn.cols;         // width
+    int h = mIn.rows;         // height
+    int nc = mIn.channels();  // number of channels
+    cout << "image: " << w << " x " << h << endl;
+
+
+
+
+    // Set the output image format
+    // ###
+    // ###
+    // ### TODO: Change the output image format as needed
+    // ###
+    // ###
+    cv::Mat mOut(h,w,mIn.type());  // mOut will have the same number of channels as the input image, nc layers
+    //cv::Mat mOut(h,w,CV_32FC3);    // mOut will be a color image, 3 layers
+    //cv::Mat mOut(h,w,CV_32FC1);    // mOut will be a grayscale image, 1 layer
+    // ### Define your own output images here as needed
+
+
+
+
+    // Allocate arrays
+    // input/output image width: w
+    // input/output image height: h
+    // input image number of channels: nc
+    // output image number of channels: mOut.channels(), as defined above (nc, 3, or 1)
+
+    // allocate raw input image array
+    float *imgIn  = new float[(size_t)w*h*nc];
+
+    // allocate raw output array (the computation result will be stored in this array, then later converted to mOut for displaying)
+    // float *imgOut = new float[(size_t)w*h*mOut.channels()];
+
+
+
+
+    // For camera mode: Make a loop to read in camera frames
+#ifdef CAMERA
+    // Read a camera image frame every 30 milliseconds:
+    // cv::waitKey(30) waits 30 milliseconds for a keyboard input,
+    // returns a value <0 if no key is pressed during this time, returns immediately with a value >=0 if a key is pressed
+    while (cv::waitKey(30) < 0)
+    {
+    // Get camera image
+    camera >> mIn;
+    // convert to float representation (opencv loads image values as single bytes by default)
+    mIn.convertTo(mIn,CV_32F);
+    // convert range of each channel to [0,1] (opencv default is [0,255])
+    mIn /= 255.f;
+#endif
+
+    // Init raw input image array
+    // opencv images are interleaved: rgb rgb rgb...  (actually bgr bgr bgr...)
+    // But for CUDA it's better to work with layered images: rrr... ggg... bbb...
+    // So we will convert as necessary, using interleaved "cv::Mat" for loading/saving/displaying, and layered "float*" for CUDA computations
+    convert_mat_to_layered (imgIn, mIn);
+
+
+    // Compute reduction
+    float *d_imgIn;
+    size_t size = pow(10, 8);
+
+    // Allocate device memory
+    cudaMalloc(&d_imgIn, size * sizeof(float)); CUDA_CHECK;
+
+    // Set all values of the input to 1.0
+    float *in  = new float[(size_t)size];
+    for (size_t i = 0; i < size; i++)
+        in[i] = 1.0;
+
+    cudaMemcpy(d_imgIn, in, size * sizeof(float), cudaMemcpyHostToDevice); CUDA_CHECK;
+
+
+    // Sequential reduction
+    float sum = 0.0;
+    Timer timer; timer.start();
+    for (size_t i = 0; i < size; i++)
+    {
+        sum += in[i];
+    }
+    timer.end();  float t = timer.get();
+    cout << "time: " << t*1000 / (float)repeats << " ms" << endl;
+    cout << "seq_sum =" << sum << endl;
+
+
+    // My reduction
+    sum = 0.0;
+    timer.start();
+    sum = cuda_reduction(d_imgIn, size);
+    timer.end(); t = timer.get();
+    cout << "time: " << t*1000 / (float)repeats << " ms" << endl;
+    cout << "my_sum =" << sum << endl;
+
+
+    // CUDA library reduction
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+    
+    sum = 0.0;
+    timer.start();
+    cublasSasum(handle, size, d_imgIn, 1, &sum); CUDA_CHECK;
+    timer.end(); t = timer.get();
+    cout << "time: " << t*1000 / (float)repeats << " ms" << endl;
+    cout << "cuda_sum =" << sum << endl;
+
+
+    // // show input image
+    // showImage("Input", mIn, 100, 100);  // show at position (x_from_left=100,y_from_above=100)
+
+    // // show output image: first convert to interleaved opencv format from the layered raw array
+    // convert_layered_to_mat(mOut, imgOut);
+    // showImage("Output", mOut, 100+w+40, 100);
+
+    // ### Display your own output images here as needed
+
+#ifdef CAMERA
+    // end of camera loop
+    }
+#else
+    // wait for key inputs
+    cv::waitKey(0);
+#endif
+
+
+
+
+    // save input and result
+    cv::imwrite("image_input.png",mIn*255.f);  // "imwrite" assumes channel range [0,255]
+    cv::imwrite("image_result.png",mOut*255.f);
+
+    // free allocated arrays
+    delete[] imgIn;
+    //delete[] imgOut;
+
+    // close all opencv windows
+    cvDestroyAllWindows();
+    return 0;
+}
+
+
+
